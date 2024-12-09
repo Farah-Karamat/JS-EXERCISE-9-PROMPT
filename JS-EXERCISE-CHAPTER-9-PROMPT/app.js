7b8bd4bddeb0575018f82c0e644c5e61fa80b9c9
Farah Karamat <farahkaramat@gmail.com>
2024-12-10 01:45:56 +0500

    JS-EXERCISE-9-PROMPT
    CHAPTER-9-PROMPT

---
+++ JS-EXERCISE-CHAPTER-9-PROMPT/app.js
@@ -0,0 +1,57 @@
+/*Q.1: Write a program to take “city” name as input from user. If
+user enters “Karachi”, welcome the user like this:
+“Welcome to city of lights”*/
+//Answer:
+
+// var city = "Karachi";
+// var cityName = prompt("Enter your city Name");
+// alert("Welcome to the city of lights");
+
+//============================================================================================================
+
+/*Q.2. Write a program to take “gender” as input from user. If the
+user is male, give the message: Good Morning Sir. If the
+user is female, give the message: Good Morning Ma’am.*/
+//Answer:
+
+// var gender = prompt("Enter gender");
+// if(gender == "Male"){
+// alert("Good Morning Sir")
+// };
+// else if(gender == "Female"){
+// alert("Good Morning Ma'am")
+// };
+
+//============================================================================================================
+
+/*Q.3.Write a program to take input color of road traffic signal
+from the user & show the message according to this table:*/
+//Answer:
+
+// var trafficSignal = prompt("Enter Signal Color");
+// if(trafficSignal == "Red"){
+// alert("Must Stop")
+// };
+// if(trafficSignal == "Yellow"){
+// alert("Ready to Move")
+// };
+// if(trafficSignal == "Green"){
+// alert("Move Now")
+// };
+
+//============================================================================================================
+
+ /*Q.4.Write a program to take input remaining fuel in car (in
+litres) from user. If the current fuel is less than 0.25litres,
+show the message “Please refill the fuel in your car”.*/   
+//Answer:
+
+// var fuel = prompt("remaining fuel in car (in litres)");
+// if(fuel < 0.25){
+// alert("Please refill the fuel in your car")
+// }else{
+// alert("Keep Driving") // This code I run only for my own practice, it is not a part of question
+// };
+
+//============================================================================================================
+
